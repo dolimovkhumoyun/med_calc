ac1d770a8ff4650afd1e0777b2817b1fd1047563
Khumoyunmirzo <humodalim@gmail.com>
2023-08-05 16:26:02 +0500

    minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,32 @@
 import { useState } from "react";
 
 const App = () => {
-	const [hemo, setHemo] = useState("")
+	const [hemo, setHemo] = useState("");
 	const [elektro, setElektro] = useState("");
 	const [result, setResult] = useState("");
-	const [customErr, setCustomErr] = useState("")
+	const [customErr, setCustomErr] = useState("");
 
-	
 	const calculate = (type: string, event: any) => {
 		let hemoTmp = hemo;
 		let elektroTmp = elektro;
-		if(type === "elektro") {
-			elektroTmp = event.target.value
-			setElektro(event.target.value)
-		} else if(type === "hemo"){
-			hemoTmp = event.target.value
-			setHemo(event.target.value)
+		if (type === "elektro") {
+			elektroTmp = event.target.value;
+			setElektro(event.target.value);
+		} else if (type === "hemo") {
+			hemoTmp = event.target.value;
+			setHemo(event.target.value);
 		}
 		const upper = Number(hemoTmp) * 3;
 		const lower = Number(elektroTmp) * 100;
-		if(typeof lower === "number" && lower === 0) {
-			setCustomErr("Рақамлар нотўғри киритилган! ")
+		if (typeof lower === "number" && lower === 0) {
+			setCustomErr("Рақамлар нотўғри киритилган! ");
 		} else {
 			let result = (upper / lower).toFixed(2);
-			console.log(result)
+			console.log(result);
 			setResult(result);
-			setCustomErr("")
+			setCustomErr("");
 		}
-	}
+	};
 
 	return (
 		<div className="bg-white  px-8 pt-6 pb-8 mb-4">
@@ -56,7 +55,9 @@ const App = () => {
 					Эритроцитлар сони
 				</label>
 				<input
-					className={`shadow appearance-none border ${customErr ? "border-red-500": ""} rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline`}
+					className={`shadow appearance-none border ${
+						customErr ? "border-red-500" : ""
+					} rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline`}
 					id="eritrotsit"
 					type="number"
 					placeholder="Эритроцитлар сони"
@@ -73,14 +74,19 @@ const App = () => {
 					Натижа
 				</label>
 				<input
-					className={`shadow appearance-none border ${customErr ? "border-red-500": ""} rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline`}
+					className={`shadow appearance-none border ${
+						customErr ? "border-red-500" : ""
+					} rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline`}
 					id="result"
 					type="number"
-					value={result}	
+					value={result}
 					readOnly
 				/>
 			</div>
 			{customErr && <p className="text-red-500 text-xs italic">{customErr}</p>}
+			<p className="text-center text-gray-500 text-xs">
+				&copy; Made by Khumoyunmirzo Dolimov.
+			</p>
 		</div>
 	);
 };
